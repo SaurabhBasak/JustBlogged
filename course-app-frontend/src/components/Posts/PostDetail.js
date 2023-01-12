a8d6ca6fb2b0a9efef51fdf369896df52895a1a1
Saurabh Basak <saurabhbasak1@gmail.com>
2023-01-12 18:13:18 -0500

    untracked files

---
+++ course-app-frontend/src/components/Posts/PostDetail.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import CommentsList from "../Comments/CommentsList";
+import CommentForm from "../Comments/CommentForm";
+
+const PostDetail = () => {
+  const [isCommenting, setIsCommenting] = useState(false);
+  const params = useParams();
+  let [post, setPost] = useState({});
+  const [commentsList, setCommentsList] = useState([]);
+
+  useEffect(() => {
+    const getPost = async () => {
+      const response = await fetch(
+        `http://127.0.0.1:8000/posts/${params.post_id}`
+      );
+      post = await response.json();
+      setPost(post);
+      setCommentsList(post.comments);
+    };
+
+    getPost();
+  }, []);
+
+  const addCommentToList = (commentBody) => {
+    setCommentsList((prevCommentList) => {
+      const updatedList = [...prevCommentList];
+      updatedList.unshift(commentBody);
+      return updatedList;
+    });
+  };
+
+  const createCommentHandler = async (commentBody) => {
+    const response = await fetch(
+      `http://127.0.0.1:8000/posts/${params.post_id}`,
+      {
+        method: "POST",
+        body: JSON.stringify(commentBody),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const comment = await response.json();
+    addCommentToList(comment);
+  };
+
+  const startCommentingHandler = () => {
+    setIsCommenting(true);
+  };
+
+  const stopCommentingHandler = () => {
+    setIsCommenting(false);
+  };
+
+  return (
+    <section>
+      <Link to="/posts">Home</Link>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      {!isCommenting && (
+        <button onClick={startCommentingHandler}>Comment</button>
+      )}
+      {isCommenting && (
+        <CommentForm
+          post_id={params.post_id}
+          createComment={createCommentHandler}
+          onCancel={stopCommentingHandler}
+        />
+      )}
+      <CommentsList post_id={params.post_id} comments={commentsList} />
+    </section>
+  );
+};
+
+export default PostDetail;
